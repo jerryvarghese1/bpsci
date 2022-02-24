ac68e03ad9e0ea4ecad5d2ddea300cff2d056a04
Jerry Varghese <63359305+jerryvarghese1@users.noreply.github.com>
2022-02-24 15:30:26 +0100

    Update orbital_intercept_blend.py

--- examples/orbital_intercept/orbital_intercept_blend.py
+++ examples/orbital_intercept/orbital_intercept_blend.py
@@ -5,15 +5,15 @@ import pandas as pd
 
 from scipy.spatial.transform import Rotation as R 
 
-file_loc = "./examples/orbital_intercept/orb_int_data.csv"
+file_loc = "./examples/orbital_intercept/orb_int_data.csv" # read data
 data = pd.read_csv(file_loc)
 
-euler_pa = [0, 0, 0]
-euler_type = 'xyz'
+euler_pa = [0, 0, 0] # offset of principal axes (from the inertia tensor). In this case, all the objects are strictly translating, so this will be passed to all the dynamics objects indicating that there is no offset of the principal axes
+euler_type = 'xyz' # same as previous
 
-anim = bpsc.init_anim(data['t'].to_numpy(), 4000, 1/1000)
+anim = bpsc.init_anim(data['t'].to_numpy(), 4000, 1/1000) #initialize animation using the time vector, speeding up the simulation 4000x and scaling it to 1/1000 of the original size
 
-asteroid = bpsc.dyn_obj(bpy.data.objects['asteroid'], euler_pa, euler_type, None, anim)
+asteroid = bpsc.dyn_obj(bpy.data.objects['asteroid'], euler_pa, euler_type, None, anim) # the asteroid's dynamic object
 asteroid.apply_animation(data['t_x'].to_numpy(), data['t_y'].to_numpy(), data['t_z'].to_numpy(), None)
 asteroid.apply_streamline('dynamic', data['t_x'].to_numpy(), data['t_y'].to_numpy(), data['t_z'].to_numpy(), .1)
 
