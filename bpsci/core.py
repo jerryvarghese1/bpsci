a44e5e5c4b639e5eb66799d4c378078d1d982aeb
Jerry Varghese <63359305+jerryvarghese1@users.noreply.github.com>
2022-04-27 21:37:48 +0200

    replace with new docstrings

--- bpsci/core.py
+++ bpsci/core.py
@@ -1,3 +1,10 @@
+"""
+Building Blocks of a Dynamic Visualization - :mod:`bpsci.core`
+==============================================================
+The bpsci core is a collection of classes and methods used to create dynamic visualizations.
+
+"""
+
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from scipy.interpolate import interp1d
@@ -5,29 +12,63 @@ import bpy
 import pandas as pd
 
 class init_anim:
+    """
+    Sets up the global animation information such speed up and global scale
+
+    :param np.ndarray t: contains the time information that corresponds with the six degrees of freedom data 
+    :param float speed_up: the 'real time speed up' or the ratio of the duration of the data to the duration of the animation 
+    :param float scale: global physical scale factor of the animation, i.e., .1 will reduce everything to be 1/10th its original size
+    """
     def __init__(self, t, speed_up, scale):
+
         self.frame_rate = bpy.context.scene.render.fps
+        """:type: `float`: frame rate of animation"""
         
         self.t = t
+        """:type: `np.ndarray`: contains the time information that corresponds with the six degrees of freedom data"""
         
         self.speed_up = speed_up
+        """:type: `float`: the "real time speed up" or the ratio of the duration of the data to the duration of the animation"""
         
         self.frames = np.linspace(1, int(t[-1]*self.frame_rate/speed_up+1), len(t)).astype(int)
+        """:type: `np.ndarray`: the frames that Blender will animate and have corresponding data for"""
         
         self.frame_duration = self.frames[-1]
+        """:type: `int`: number of total frames in animation"""
 
         self.scale = scale
+        """:type: `float`: global physical scale factor of the animation, i.e., .1 will reduce everything to be 1/10th its original size"""
         
         bpy.context.scene.frame_start = 1
         bpy.context.scene.frame_end = self.frame_duration+1
 
 class ref_frame:
+    """
+    Initializes a reference frame that other objects and animations can be the child of. 
+
+    In blender, this takes the form of an Empty object.
+
+
+    :param str name: the Blender object name of the reference frame
+    :param bpy.data.object parent: the Blender object parent of this reference frame 
+    :param anim: class object that was used to initialize the animation
+    :type anim: :class:`bpsci.core.anim`
+    
+
+    """
     def __init__(self, name, parent, anim):
+
+        
+        
         self.name = name
+        """:type: `str`: the Blender object name of the reference frame"""
         self.parent = parent
+        """:type: `bpy.data.object`: the Blender object parent of this reference frame"""
         
         # Blender portion
         self.ob = bpy.data.objects.new(name, None)
+        """:type: `bpy.data.object`: the Blender object that represents this reference frame"""
+        
         bpy.context.scene.collection.objects.link(self.ob)
         
         self.ob.parent = parent
@@ -35,10 +76,29 @@ class ref_frame:
         self.ob.rotation_mode = 'QUATERNION'
 
         self.frames = anim.frames
+        """:type: `np.ndarray`: the frames that Blender will animate and have corresponding data for"""
+
         self.scale = anim.scale
+        """:type: `float`: global physical scale factor of the animation"""
 
         
     def static_6DOF(self, quat, x, y, z):
+        """
+        Places the reference frame in one specified static location.
+
+        .. warning::
+            This is currently an internal method. This method will be exposed properly in future updates
+        .. versionadded:: 0.2.30
+
+        May be useful for offsetting the reference frame from the parent object (i.e. for center of mass of principal axes offset)
+
+
+        :param np.ndarray quat: an numpy array of one quaternion
+        :param float x: the x position 
+        :param float y: the y position 
+        :param float z: the z position 
+
+        """
         if not(quat is None):
             self.ob.rotation_quaternion[1] = quat[0]
             self.ob.rotation_quaternion[2] = quat[1]
@@ -50,6 +110,22 @@ class ref_frame:
             self.ob.location = (x, y, z)
         
     def dynamic_6DOF(self, quat, x_list, y_list, z_list):
+        """
+        Animates the reference frame over time.
+
+        .. warning::
+            This is currently an internal method. This method will be exposed properly in future updates
+        .. versionadded:: 0.2.30
+
+        May be useful for continually offsetting the reference frame from the parent object (i.e. for center of mass of principal axes offset for changing mass/distribution)
+
+        :param np.ndarray quat: an numpy array of quaternions over time
+        :param np.ndarray x: the x position over time
+        :param np.ndarray y: the y position over time
+        :param np.ndarray z: the z position over time
+
+        """
+
         frames = self.frames
         if not(quat is None):
             for i in range(len(frames)):
@@ -77,21 +153,48 @@ class ref_frame:
         
 
 class dyn_obj:
+    """
+    The main building block of all dynamic visualizations 
+
+
+    :param bpy.data.object obj: the Blender object that will be animated
+    :param np.ndarray pa: the offset of the principal axes specified by the :param: ` euler_type:
+    :param str euler_type: the Euler angle order (i.e. 'xyz' for 1,2,3 or 'zxz' for 3,1,3)
+    :param bpy.data.object parent: the Blender object parent of the original object 
+    :param anim: class object that was used to initialize the animation
+    :type anim: :class:`bpsci.core.init_anim`
+        
+    """
     def __init__(self, obj, pa, euler_type, parent, anim):
+        
+
+        self.parent = parent
+        """:type: `bpy.data.object`: the Blender object parent of this reference frame"""
 
         self.frames = anim.frames
+        """:type: `np.ndarray`: the frames that Blender will animate and have corresponding data for"""
         self.scale = anim.scale
+        """:type: `float`: global physical scale factor of the animation"""
+
+        self.ob = obj
+        """:type: `bpy.data.object`: the Blender object that will be animated"""
         
         self.euler_pa = pa
+        """:type: `np.ndarray`: the offset of the principal axes specified by the :param: ` euler_type:"""
         
         self.quat = R.from_euler(euler_type, pa).as_quat()
+        """:type: `np.ndarray`: a numpy array of one quaternion that represents the principal axes offset"""
         
         self.non_rot = ref_frame(obj.name+'_non_rot', parent, anim)
+        """:class:`~bpsci.core.ref_frame`: the non-rotational reference frame (owner of translational movement only)"""
         
         self.pa_axes = ref_frame(obj.name+'_pa', self.non_rot.ob, anim)
+        """:class:`~bpsci.core.ref_frame`: the principal axes rotational reference frame (owner of rotational movement, inherits translational movement from parent)"""
+
         self.pa_axes.static_6DOF(self.quat, None, None, None)
         
         self.body = ref_frame(obj.name+'_body', self.pa_axes.ob, anim)
+        """:class:`~bpsci.core.ref_frame`: the body reference frame (untransformed principal axes)"""
         
         obj.parent = self.body.ob
         obj.scale = (self.scale, self.scale, self.scale)
@@ -99,16 +202,38 @@ class dyn_obj:
         self.body.static_6DOF(-self.quat, None, None, None)
 
         self.name = obj.name
-        
+        """:type: `str`: the Blender object name of the original object"""
+
     def apply_animation(self, x_list, y_list, z_list, quat_list):
-        
+
+        """
+        Animates a :class:`~bpsci.core.dyn_obj` in the full six degrees of freedom
+
+        :param np.ndarray x_list: a numpy array of the x position over time
+        :param np.ndarray y_list: a numpy array of the y position over time
+        :param np.ndarray z_list: a numpy array of the z position over time
+        :param np.ndarray quat_list: a numpy array of the quaternion over time. Can be passed None if rotation is ignored.
+        """
 
         self.pa_axes.dynamic_6DOF(quat_list, None, None, None)
         self.non_rot.dynamic_6DOF(None, x_list, y_list, z_list)
 
     def apply_streamline(self, staticity, int_x, int_y, int_z, thickness):
+        """
+        Creates a static or dynamic positional streamline for a :class:`~bpsci.core.dyn_obj`
+
+        :param str staticity: a string (either 'static' or 'dynamic') that specifies whether the the streamline is animated over time
+        :param np.ndarray int_x: a numpy array of the x position over time
+        :param np.ndarray int_y: a numpy array of the y position over time
+        :param np.ndarray int_z: a numpy array of the z position over time
+        
+        """
+
+
         name = self.name+'_streamline'
+        """:type: `str`, the Blender object name of the streamline"""
         frames = self.frames
+        """:type: `np.ndarray`: the frames that Blender will animate and have corresponding data for"""
         
         int_size = len(int_x)
         int_coords = np.zeros((int_size, 3))
@@ -139,25 +264,49 @@ class dyn_obj:
 
         int_curveOB.data.bevel_factor_start = 0
 
+        curve_to_mesh(int_curveOB)
+
         if staticity == 'dynamic':
-            for i in range(len(frames)):
-                cur_frame = frames[i]
-                
-                bpy.data.objects[name].data.bevel_factor_end = 1/(frames[-1]) * cur_frame
-                bpy.data.objects[name].data.keyframe_insert(data_path='bevel_factor_end', frame=cur_frame)
+            bpy.ops.object.modifier_add(type='BUILD')
+            bpy.context.object.modifiers["Build"].frame_start = 0
+            bpy.context.object.modifiers["Build"].frame_duration = self.frames[-1]+1
+
 
 class dyn_vec:
-    def __init__(self, obj, name, scale_mag, scale_off, offset, anim):
-        self.parent = obj
+    """
+    Initializes a dynamic vector`
+
+
+    :param bpy.data.object parent: the Blender object the vector is associated with
+    :param str name: the Blender object name of the vector ('_vector' will be appended to this name)
+    :param float scale_mag: the scaling factor of the vector's magnitude axis (purely aesthetic)
+    :param float scale_off: the scaling factor of the vector's off-axes (purely aesthetic)
+    :param tuple[float] offset: offset of vector from the parent object (purely aesthetic)
+    :param anim: class object that was used to initialize the animation
+    :type anim: :class:`bpsci.core.anim`
+
+    """
+    def __init__(self, parent, name, scale_mag, scale_off, offset, anim):
+
+        
+
+        self.parent = parent
+        """:type: `bpy.data.object`: the Blender object parent of this reference frame"""
+
         self.name = name+"_vector"
+        """str: the Blender object name of the vector"""
         
         self.parent_rf = ref_frame(self.name+"_empty", self.parent, anim)
+        """:type: `bpy.data.object`: holds the vector"""
 
         self.filepath = self.filepath = __file__.replace("__init__.py", "").replace("\\core.py", "")+'\\assets\\objects\\arrow.obj' #"C:/Users/vargh/Documents/GitHub/bpsci/bpsci/assets/objects/arrow.obj" #bpsci.__file__.replace("__init__.py", "")+'\\assets\\objects\\arrow.obj'
+        """:type: `str`: file path to arrow asset"""
+
 
         vec = bpy.ops.import_scene.obj(filepath=self.filepath)
         vec = bpy.context.selected_objects[0]
         self.vec = vec
+        """:type: `bpy.data.object`: the actual arrow asset"""
         
         self.parent_rf.ob.location = offset
         vec.parent = self.parent_rf.ob
@@ -166,9 +315,20 @@ class dyn_vec:
         self.scale = scale_mag
         vec.scale = (scale_mag, scale_off, scale_off)
         self.anim = anim
+        """:class:`bpsci.core.init_anim`: class object that was used to initialize the animation"""
         
         self.frames = anim.frames
+        """:type: `np.ndarray`: the frames that Blender will animate and have corresponding data for"""
     def animate(self, x, y, z):
+        """
+        Animates a dynamic vector
+
+        :param np.ndarray x: a numpy array of the x component over time
+        :param np.ndarray y: a numpy array of the y component over time
+        :param np.ndarray z: a numpy array of the z component over time
+
+        """
+
         frames = self.frames
 
         max_x = 1
@@ -187,6 +347,7 @@ class dyn_vec:
         norm_z = z/max_z *self.scale
         
         self.point_rf = ref_frame(self.name+"_pointing_empty", self.parent, self.anim)
+        """:type: `bpy.data.object`: an empty that provides calculates the directionality of the vector"""
         
         tracking_constraint = self.parent_rf.ob.constraints.new('DAMPED_TRACK')
         tracking_constraint.target = self.point_rf.ob
@@ -208,7 +369,23 @@ class dyn_vec:
             self.parent_rf.ob.keyframe_insert(data_path='scale', frame=cur_frame)
 
 class anim_text:
+    """
+    Creates animated text
+
+
+    :param str name: the Blender object name of the text ('_text' will be appended to this name)
+    :param bpy.data.object parent: the Blender object parent of this text 
+    :param anim: class object that was used to initialize the animation
+    :type anim: :class:`bpsci.core.anim`
+    :param np.ndarray data: the data to be displayed (same length as the number of animated frames)
+    :param str label: a label to append to the text (if no label is desired, pass '')
+    :param int fix_place: the number of decimal places to fix the text to
+    :param bool if_str: whether or not the data is a numerical or string vector. Currently only numerical vectors are supported.
+
+    """
     def __init__(self, name, parent, anim, data, label, fix_place, if_str):
+
+        
 
         self.anim = anim
 
@@ -222,7 +399,7 @@ class anim_text:
 
         self.obj.parent = parent
         self.name = name
-        
+
         all_frames = np.arange(1, anim.frames[-1])
         frame_interper = interp1d(anim.frames, data, fill_value = 'extrapolate')
 
@@ -234,14 +411,27 @@ class anim_text:
                 cur_frame = bpy.context.scene.frame_current
                 
                 text.data.body = ('{0:.'+str(fix_place)+'f}').format(fixed_data[cur_frame-1]) + ' ' + label
-
-        else:
-            def recalculate_text(scene):
-                text = ob
-                cur_frame = bpy.context.scene.frame_current
-                
-                text.data.body = data + ' ' + label
                     
 
             bpy.app.handlers.frame_change_pre.append(recalculate_text)
+
+def curve_to_mesh(curve):
+    context = bpy.context
+    deg = context.evaluated_depsgraph_get()
+    me = bpy.data.meshes.new_from_object(curve.evaluated_get(deg), depsgraph=deg)
+    
+    curve_name = curve.name
+
+    new_obj = bpy.data.objects.new(curve.name + "_mesh", me)
+    context.collection.objects.link(new_obj)
+
+    for o in context.selected_objects:
+        o.select_set(False)
+
+    new_obj.matrix_world = curve.matrix_world
+    new_obj.select_set(True)
+    context.view_layer.objects.active = new_obj
+    
+    bpy.data.objects.remove(curve, do_unlink=True)
+    new_obj.name = curve_name
 
--- bpsci/utils.py
+++ bpsci/utils.py
@@ -4,6 +4,16 @@ import bpy
 import pandas as pd
 
 def erase_others(obj_name):
+    """
+        Erase all objects associated with the passed object name. 
+
+        .. warning::
+            this function will delete any object that has the passed object name as a substring
+        .. versionadded:: 0.2.30
+
+        :param str obj_name: the object name
+
+        """
     actual_obj = bpy.data.objects[obj_name]
     name = actual_obj.name
     
@@ -26,4 +36,28 @@ def erase_others(obj_name):
     [bpy.data.objects.remove(bpy.data.objects[obj_name]) for obj_name in relatives]
 
 def bpy_obj(obj_name):
+    """
+        Returns the Blender object associated with the passed name
+
+        :param str obj_name: the object name
+
+        """
     return bpy.data.objects[obj_name]
+
+def euler2quat(angles1, angles2, angles3, euler_type):
+    """
+        Takes a set of Euler angles over time and returns the equivalent quaternion representation
+
+        :param np.ndarray angles1: first set of angles over time
+        :param np.ndarray angles2: second set of angles over time
+        :param np.ndarray angles3: third set of angles over time
+        :param str euler_type: the Euler angle order (i.e. 'xyz' for 1,2,3 or 'zxz' for 3,1,3)
+
+        Returns:
+            :returns (nd.array): a numpy array of quaternions over time
+
+        """
+
+    all_angles = np.vstack([angles1, angles2, angles3]).transpose()
+    quat_out = R.from_euler(euler_type, all_angles).as_quat()
+    return quat_out
