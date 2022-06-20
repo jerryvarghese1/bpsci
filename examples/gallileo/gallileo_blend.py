3f2e4e530349c3b0eaa16f55f09f39cf691da390
Jerry Varghese <63359305+jerryvarghese1@users.noreply.github.com>
2022-06-20 19:25:31 +0200

    Update gallileo_blend.py
    new API update

--- examples/gallileo/gallileo_blend.py
+++ examples/gallileo/gallileo_blend.py
@@ -7,6 +7,7 @@ from bpsci import core as bpsci_core
 
 euler_pa = [.1, 0, 0] # this offsets the inertial principle axes according to the euler angle sequence below
 euler_type = 'xyz' # principal axes offset euler angle sequence
+cog_offset = [.1, 0, 0] # this offsets the inertial center of gravity
               
 file_path = './examples/gallileo/gallileo.csv' # file path of data
 
@@ -24,12 +25,11 @@ z = data['z'].to_numpy() # positional data, z
 
 # convert euler angles to quaternion (apply_animation method accepts a quaternion generated from scipy's Rotation library) 
 # if you are interested in why this transformation is necessary, look up gimbal lock and its effects on animation
-euler_angle = np.vstack([psi, theta, phi]).transpose()
-angles = R.from_euler('zxz', euler_angle).as_quat()
+euler_angle = euler2quat(np.vstack([psi, theta, phi]).transpose(), 'zxz')
              
 speed_up = 3 # the simulation will be this many times faster than real time
 scaler = 1 # if you have a very large bounding box for your movement, it may make sense to scale it down to a reasonable size to be easily viewable in Blender
 anim = bpsci_core.init_anim(t, speed_up, scaler) # the anim object initializes Blender and calculates properties that the animation will require (like frames)
 
-craft = bpsci_core.dyn_obj(bpy.data.objects['gallileo'], euler_pa, euler_type, None, anim)  #the dynamic object that represents the Gallileo spacecraft
+craft = bpsci_core.dyn_obj(bpy.data.objects['gallileo'], euler_pa, cog_offset, euler_type, None, anim)  #the dynamic object that represents the Gallileo spacecraft
 craft.apply_animation(x, y, z, angles) # apply the animation to the spacecraft
